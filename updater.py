cd0538651225d76e7c788d26ed8e108fc1ea7260
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-06-13 11:50:07 -0300

    Remote updater url

--- updater.py
+++ updater.py
@@ -1,4 +1,4 @@
 import sys, os
 
-remote = "https://github.com/luiseduardobrito/OpenRefine/raw/gpnx/launcher"
+remote = "https://github.com/luiseduardobrito/OpenRefine/raw/gpnx/refine.py"
 os.system("wget -N %s" % remote)

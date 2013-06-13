6819e4ea3310a2e9d4daea28cc0e1fd265743b5f
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-06-13 10:29:27 -0300

    Launcher parsing methods

--- launcher.py
+++ launcher.py
@@ -3,24 +3,51 @@ from xml.dom import minidom
 
 class Launcher:
 
-	_config = "https://gist.github.com/luiseduardobrito/5750844/raw/3216b5c8243aae0c717bc7d4826836a66ae3dbe3/config.xml"
+	_remote = "https://gist.github.com/luiseduardobrito/5750844/raw/3216b5c8243aae0c717bc7d4826836a66ae3dbe3/config.xml"
+	_version = "0.1"
+
+	def getText(self, nodelist):
+	    rc = []
+	    for node in nodelist:
+	        if node.nodeType == node.TEXT_NODE:
+	            rc.append(node.data)
+	    return ''.join(rc)
+
+	def handleTok(self, tokenlist):
+	    texts = ""
+	    for token in tokenlist:
+	        texts += " "+ self.getText(token.childNodes)
+	    return texts
 
 	def _log(self, msg):
 		print msg
 
 	def check_updates(self, config_url = ''):
 		self._log("Getting update information from server...")
-		self._log("Client already in latest version.")
+		config = self.parse(self._remote)
+		self._log("Client version: %s" % self._version)
+
+	def parse(self, input):
+		xmldoc = minidom.parse(urllib.urlopen(input))
+		config = xmldoc.getElementsByTagName("config")[0]
+
+		if(config):
+			self._version = self.handleTok(config.getElementsByTagName("version"))
+		else:
+			self._log("No configuration information found, downloading new one...");
+			os.system("wget %s" % self._remote)
+			check_updates()
 
 	def run(self):
 		self._log("Starting OpenRefine jetty server...")
-		os.system('. ../refine')
+		#os.system('./refine')
 
 	def __init__(self):
-		print "Initializing launcher..."
-		if(len(sys.argv) > 1 and sys.argv[1] != "--skip-update-check"):
-			self._config = sys.argv[1]
-			check_updates()
+		self._log("GPNX Refine - Initializing launcher resources...")
+		if(len(sys.argv) > 1 and sys.argv[1] == "--skip-update-check"):
+			self.run()
+			return
+		self.check_updates()
 		self.run()
 
 l = Launcher()
